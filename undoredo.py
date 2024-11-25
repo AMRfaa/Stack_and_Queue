d6a57a0f66fc794ca8e8645b4a79677ff48f3bbc
Anisa Muziya Rafa <amrxll08@gmail.com>
2024-11-25 16:01:01 +0800

    undoredo.py

--- undoredo.py
+++ undoredo.py
@@ -1,33 +1,33 @@
 undo_stack = []
 redo_stack = []
 
-def add_action(action):
-    undo_stack.append(action)
-    redo_stack.clear()  # Kosongkan redo stack setiap kali ada aksi baru
-    print(f"Aksi '{action}' ditambahkan.")
+def tambah_aksi (aksi):
+    undo_stack.append(aksi)
+    redo_stack.clear()  
+    print(f"Aksi '{aksi}' ditambahkan.")
     print("Data setelah di-Undo Stack:", undo_stack)
     print("Data setelah di-Redo Stack:", redo_stack)
 
 def undo():
     if undo_stack:
-        action = undo_stack.pop()
-        redo_stack.append(action)
-        print(f"Undo dilakukan: '{action}'.")
+        aksi = undo_stack.pop()
+        redo_stack.append(aksi)
+        print("Undo dilakukan: ",aksi,".")
     else:
         print("Tidak ada aksi untuk di-undo!")
-    print("Data setelah di-Undo Stack:", undo_stack)
-    print("Data setelah di-Redo Stack:", redo_stack)
+        print("Data setelah di-Undo Stack:", undo_stack)
+        print("Data setelah di-Redo Stack:", redo_stack)
 
 def redo():
     if redo_stack:
-        action = redo_stack.pop()
-        undo_stack.append(action)
-        print(f"Redo dilakukan: '{action}'.")
+        aksi = redo_stack.pop()
+        undo_stack.append(aksi)
+        print("Redo dilakukan: ",aksi,".")
     else:
         print("Tidak ada aksi untuk di-redo!")
-    print("Data setelah di-Undo Stack:", undo_stack)
-    print("Data setelah di-Redo Stack:", redo_stack)
-
+        print("Data setelah di-Undo Stack:", undo_stack)
+        print("Data setelah di-Redo Stack:", redo_stack)
+        
 def menu():
     print("\n MENU ")
     print("1. Tambah Data")
@@ -38,9 +38,10 @@ def menu():
 while True:
     menu()
     pilihan = input("Pilih menu (1-4): ")
+
     if pilihan == "1":
         aksi = input("Masukkan data yang ingin dimasukkan: ")
-        add_action(aksi)
+        tambah_aksi (aksi)
     elif pilihan == "2":
         undo()
     elif pilihan == "3":
